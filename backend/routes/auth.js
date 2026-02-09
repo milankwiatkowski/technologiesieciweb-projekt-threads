e3614caec5c21da2897cd3ddc43ed031914c2407
milankwiatkowski <milan.mikolaj.kwiatkowski@gmail.com>
2026-02-09 18:43:30 +0100

    auth fix

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -68,8 +68,6 @@ router.post('/login', async (req, res,next) => {
                 else{
                     console.log(`INFO User ${req.body.login} tried logging in but failed due to an incorrect password ${getTime()}`)
                     return res.status(501).json({messaage:"Incorrect password"})
-                    console.log(`INFO User ${req.body.login} tried logging in but failed due to an incorrect password ${time}`)
-                    return res.status(501).json({message:"Incorrect password"})
                 }
             }
             else{
